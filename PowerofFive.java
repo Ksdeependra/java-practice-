e9c43c7bfc61155432ea3b55fd994a7d1083dad7
Ksdeependra <46922710+Ksdeependra@users.noreply.github.com>
2019-01-23 16:21:06 +0530

    Add files via upload

---
+++ PowerofFive.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+public class PowerofFive {
+
+	public static void main(String[] args) {
+		System.out.println("enter the number");
+		int n,i,k;
+		Scanner sc=new Scanner(System.in);
+		n=sc.nextInt();
+		int flag=0;
+		for(i=1;i<=n;i++)
+		{
+			double p=Math.pow(5,i);
+			if((int)p==n)
+			{
+				flag=1;
+				break;
+			}
+			else
+			{
+				flag=0;
+			}
+        }
+		
+		if(flag==1)
+		{
+			System.out.println("True");
+		}
+		else
+		{
+			System.out.println("False");
+		}
+	}
+
+}
+
+
